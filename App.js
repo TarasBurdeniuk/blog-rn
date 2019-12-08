cb59a59bb037bd1d59147344ee6fcd34e38ba326
Taras Burdeniuk <bur.taras@gmail.com>
2019-12-08 17:05:30 +0200

    added eslint and prettier

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {AppLoading} from 'expo';
+import React, { useState } from 'react';
+import { Text, View, Alert } from 'react-native';
+import { AppLoading } from 'expo';
+import bootstrap from './src/bootstrap';
 
 export default function App() {
     const [isReady, setIsReady] = useState(false);
     if (!isReady) {
-        return <AppLoading
-            onFinish={() => setIsReady(true)}
-            onError={(err) => console.error(err)}
-        />
+        return (
+            <AppLoading
+                startAsync={bootstrap}
+                onFinish={() => setIsReady(true)}
+                onError={err => Alert.alert('Error', `${err}`)}
+            />
+        );
     }
 
     return (
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,10 @@
-export async function bootstrap() {
+import * as Font from 'expo-font';
 
+async function bootstrap() {
+    await Font.loadAsync({
+        'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+        'open-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+    });
 }
+
+export default bootstrap;
